9318c55b164833d9f602e6e85d96dd81db219b9b
yuuisang <yuuisang@DESKTOP-VVP14CR>
2020-10-27 04:59:37 +0900

    #의상 10/27 04:59 command, dao, vo 기본틀 작성 및 분담

--- src/vo/VO.java
+++ src/vo/VO.java
@@ -15,20 +15,14 @@ public class VO {
 			+ "(SEQ_tb_board_board_no.NEXTVAL, ?, ?, ?, SYSDATE)";
 	
 	
-	////////// 이밑은 다 수정해야함 폼만 몇개 만들어둔것!
 	public static final String SQL_WRITE_SELECT = 
-			"INSERT INTO TB_BOARD"
-			+ "(board_no, board_title, board_content, board_viewCnt, board_regdate) " 
-			+ "VALUES"
-			+ "(SEQ_tb_board_board_no.NEXTVAL, ?, ?, ?, SYSDATE)";
+			"SELECT * FROM tb_board ORDER BY board_no DESC";
 	
 	
 	public static final String SQL_WRITE_SELECT_BY_UID = 
-			"INSERT INTO TB_BOARD"
-			+ "(board_no, board_title, board_content, board_viewCnt, board_regdate) " 
-			+ "VALUES"
-			+ "(SEQ_tb_board_board_no.NEXTVAL, ?, ?, ?, SYSDATE)";
+			"SELECT * FROM tb_board WHERE board_no=?";
 
+	////////// 이밑은 다 수정해야함 폼만 몇개 만들어둔것! ////////////////////////////
 	
 	public static final String SQL_WRITE_INC_VIEWCNT = 
 			"INSERT INTO TB_BOARD"
