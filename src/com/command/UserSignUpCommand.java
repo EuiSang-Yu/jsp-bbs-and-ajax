885f6f4d418c261b974a412e4de9ad8c3767c6f1
이슬아 <@DESKTOP-3OGR79T>
2020-10-27 23:39:15 +0900

    슬아 회원가입 커밋

--- src/com/command/UserSignUpCommand.java
+++ src/com/command/UserSignUpCommand.java
@@ -7,35 +7,38 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.DAO;
 
-public class UserSignUpCommand implements Command{
+public class UserSignUpCommand implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		int cnt = 0;
 		DAO dao = new DAO();
-		
-		//입력한 값 (parameter) 받아오기
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		int viewCnt = 0;
-		
-		System.out.println("board_title : "+title +" , board_content : " + content);
-		
-		//유효성 체크
-		if(title != null && content != null &&
-				title.trim().length() > 0 && content.trim().length() > 0) {
+
+		//회원정보 값 받아오기
+		String signUpID = request.getParameter("signUpID");
+		String signUpPW = request.getParameter("signUpPW");
+		String signUpName = request.getParameter("signUpName");
+		String signUpEmail = request.getParameter("signUpEmail");
+		String signUpNum = request.getParameter("signUpNum");
+
+		System.out.println("signUpID : " + signUpID + " , " +
+		"signUpPW : " + signUpPW + " , " +
+				"signUpName : "+ signUpName + " , " +
+				"signUpEmail : " + signUpEmail + " ," +
+				"signUpNum : " + signUpNum);
+
+	
+	
+
 			try {
-				cnt = dao.insert(title, content, viewCnt);
+				cnt = dao.signUp(signUpID, signUpPW, signUpName, signUpEmail, signUpNum);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}//end if
 		
-		
-		request.setAttribute("result", cnt);
-		
+
+		request.setAttribute("result", cnt); //signupOK 결과값 request
+
 	}
-	
-	
 
 }
