46f9b6b4dea9e385dedcff1b8e0a9a5e998e58b1
yuuisang <yuuisang@DESKTOP-VVP14CR>
2020-10-30 00:27:11 +0900

    #충돌 제어

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -229,11 +229,8 @@ public class DAO {
 			pstmt.setInt(2, board_champion);
 			rs = pstmt.executeQuery();
 			arr = createArray(rs);
-			
-<<<<<<< HEAD
-=======
+
 			System.out.println("arr : " + arr.toString()+"확인할거야2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
->>>>>>> branch 'master' of https://github.com/devYoooo/OP_IT.git
 			
 			conn.commit();
 		} catch (SQLException e) {
