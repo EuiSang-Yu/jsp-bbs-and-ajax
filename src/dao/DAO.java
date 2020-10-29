a889fb9e702fc4bd8771f788763c7ad200d2cdff
yuuisang <yuuisang@DESKTOP-VVP14CR>
2020-10-30 01:22:55 +0900

    ##

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -230,14 +230,9 @@ public class DAO {
 			rs = pstmt.executeQuery();
 			arr = createArray(rs);
 
-<<<<<<< HEAD
-			System.out.println("arr : " + arr.toString()+"확인할거야2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
-=======
 			System.out.println("arr : " + arr.toString());
 
-			
->>>>>>> branch 'master' of https://github.com/devYoooo/OP_IT.git
 			conn.commit();
 		} catch (SQLException e) {
 			conn.rollback(); // 예외 발생하면 rollback
