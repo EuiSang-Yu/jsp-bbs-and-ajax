b6687aa5b3cb7555287bed3e282037747b6f9f97
yuuisang <yuuisang@DESKTOP-VVP14CR>
2020-10-29 22:21:11 +0900

    #의상

--- ERD/goldspoon.sql
+++ ERD/goldspoon.sql
@@ -6,7 +6,6 @@ DROP TRIGGER TRI_tb_reply_reply_id;
 DROP TRIGGER TRI_tb_user_user_uid;
 
 
-
 /* Drop Tables */
 
 DROP TABLE tb_attach CASCADE CONSTRAINTS;
@@ -16,44 +15,36 @@ DROP TABLE tb_board CASCADE CONSTRAINTS;
 DROP TABLE tb_user CASCADE CONSTRAINTS;
 
 
-
 /* Drop Sequences */
 
 DROP SEQUENCE SEQ_tb_board_board_id;
-DROP SEQUENCE SEQ_tb_member_member_no;
+DROP SEQUENCE SEQ_tb_like_like_id;
 DROP SEQUENCE SEQ_tb_reply_reply_id;
 DROP SEQUENCE SEQ_tb_user_user_uid;
+DROP SEQUENCE SEQ_tb_attach_attach_id;
 
-
-
-
-/* Create Sequences */
-
-CREATE SEQUENCE SEQ_tb_board_board_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_tb_reply_reply_id INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_tb_user_user_uid INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_tb_like_like_id INCREMENT BY 1 START WITH 1;
 
 
 
 /* Create Tables */
+/* Create Foreign Keys */
 
 CREATE TABLE tb_user
 (
-	user_uid number DEFAULT 0 NOT NULL,
+	user_uid number DEFAULT 0 ,
 	user_id varchar2(200) UNIQUE,
 	user_pw varchar2(200),
 	user_name varchar2(200),
 	user_email varchar2(200),
 	user_phone varchar2(200),
 	user_profileImage varchar2(200),
-	PRIMARY KEY (user_uid)
+	CONSTRAINT pk_user_user_uid PRIMARY KEY(user_uid)
 );
 
 
 CREATE TABLE tb_board
 (
-	board_id number DEFAULT 0 NOT NULL,
+	board_id number DEFAULT 0 ,
 	board_writer varchar2(200),
 	board_title varchar2(200),
 	board_content varchar2(2048),
@@ -62,77 +53,55 @@ CREATE TABLE tb_board
 	board_likeCnt number,
 	board_replyCnt number,
 	board_champion number,
-	PRIMARY KEY (board_id)
-);
-
-
-CREATE TABLE tb_attach
-(
-	-- 전체경로
-	fullName varchar2(200) NOT NULL,
-	board_id number DEFAULT 0 NOT NULL,
-	PRIMARY KEY (fullName)
+	CONSTRAINT pk_board_board_id PRIMARY KEY(board_id),
+	CONSTRAINT fk_board_board_writer FOREIGN KEY(board_writer) REFERENCES tb_user(user_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE tb_like
 (
-	like_id number NOT NULL,
+	like_id number,
 	user_uid number DEFAULT 0 NOT NULL,
 	board_id number DEFAULT 0 NOT NULL,
-	PRIMARY KEY (like_id)
+	CONSTRAINT pk_like_like_id PRIMARY KEY(like_id),
+	CONSTRAINT fk_like_user_uid FOREIGN KEY(user_uid) REFERENCES tb_user(user_uid) ON DELETE CASCADE,
+	CONSTRAINT fk_like_board_id FOREIGN KEY(board_id) REFERENCES tb_board(board_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE tb_reply
 (
-	reply_id number DEFAULT 0 NOT NULL,
+	reply_id number DEFAULT 0 ,
 	reply_writer varchar2(200),
 	reply_content varchar2(1024),
 	reply_regDate date DEFAULT sysdate,
 	board_id number DEFAULT 0 NOT NULL,
-	PRIMARY KEY (reply_id)
+	CONSTRAINT pk_reply_reply_id PRIMARY KEY(reply_id),
+	CONSTRAINT fk_reply_reply_writer FOREIGN KEY(reply_writer) REFERENCES tb_user(user_id) ON DELETE CASCADE,
+	CONSTRAINT fk_reply_board_id FOREIGN KEY(board_id) REFERENCES tb_board(board_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE tb_attach
+(
+	attach_id NUMBER DEFAULT 0 ,
+	attach_source varchar2(400),
+	board_id number DEFAULT 0 NOT NULL,
+	CONSTRAINT pk_attach_attach_id PRIMARY KEY(attach_id),
+	CONSTRAINT fk_attach_board_id FOREIGN KEY(board_id) REFERENCES tb_board(board_id) ON DELETE CASCADE
 );
 
 
 
 
+/* Create Sequences */
 
+CREATE SEQUENCE SEQ_tb_board_board_id INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE SEQ_tb_reply_reply_id INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE SEQ_tb_user_user_uid INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE SEQ_tb_like_like_id INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE SEQ_tb_attach_attach_id INCREMENT BY 1 START WITH 1 NOCACHE;
 
-/* Create Foreign Keys */
-
-ALTER TABLE tb_attach
-	ADD FOREIGN KEY (board_id)
-	REFERENCES tb_board (board_id)
-;
-
-
-ALTER TABLE tb_like
-	ADD FOREIGN KEY (board_id)
-	REFERENCES tb_board (board_id)
-;
-
-
-ALTER TABLE tb_reply
-	ADD FOREIGN KEY (board_id)
-	REFERENCES tb_board (board_id)
-;
-
-
-ALTER TABLE tb_like
-	ADD FOREIGN KEY (user_uid)
-	REFERENCES tb_user (user_uid)
-;
-
-ALTER TABLE tb_board
-	ADD FOREIGN KEY (board_writer)
-	REFERENCES tb_user (user_id)
-;
-
-ALTER TABLE tb_reply
-	ADD FOREIGN KEY (reply_writer)
-	REFERENCES tb_user (user_id)
-;
 
 
 
@@ -168,7 +137,6 @@ END;
 
 /
 
-
 CREATE OR REPLACE TRIGGER TRI_tb_user_user_uid BEFORE INSERT ON tb_user
 FOR EACH ROW
 BEGIN
@@ -177,13 +145,16 @@ BEGIN
 	FROM dual;
 END;
 
+CREATE OR REPLACE TRIGGER TRI_tb_attach_attach_id BEFORE INSERT ON tb_attach
+FOR EACH ROW
+BEGIN
+	SELECT SEQ_tb_attach_attach_id.nextval
+	INTO :new.attach_id
+	FROM dual;
+END;
+
 /
 
-
-
-/* Comments */
-
-COMMENT ON COLUMN tb_attach.fullName IS '전체경로';
 
 /* 해당 계정 테이블 목록 확인*/
 select * from tab;
