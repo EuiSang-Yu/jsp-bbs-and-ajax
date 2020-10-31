75f88ef245b3a5c1b2eb9be6b28e03eeba2b6aee
이슬아 <@DESKTOP-3OGR79T>
2020-11-01 06:47:29 +0900

    아이디찾기 비번찾기 슬아 커밋

--- src/vo/VO.java
+++ src/vo/VO.java
@@ -47,13 +47,7 @@ public class VO {
    public static final String SQL_WRITE_DELETE =
 
          "DELETE FROM TB_BOARD WHERE board_id=? and board_champion = ?";   //챔피언 넘버 추가
-<<<<<<< HEAD
 
-
-
-=======
-
->>>>>>> branch 'master' of https://github.com/devYoooo/OP_IT.git
    //--------------------------댓글 쿼리
    
    public static final String SQL_REPLY_INSERT = 
@@ -66,12 +60,9 @@ public class VO {
 		   "SELECT * FROM TB_USER WHERE USER_ID=?";	// 마이페이지 열었을때 회원가입정보 가져오기
    
    public static final String SQL_MYPAGE_UPDATE = 
-<<<<<<< HEAD
+
 	         "UPDATE TB_USER SET USER_PW = ?, USER_EMAIL = ?, USER_PHONE = ? WHERE USER_UID = ?";   //챔피언 넘버 추가
 
-=======
-	         "UPDATE TB_USER SET USER_PW = ?, USER_EMAIL = ?, USER_PHONE = ? WHERE USER_ID = ?";   //챔피언 넘버 추가
->>>>>>> branch 'master' of https://github.com/devYoooo/OP_IT.git
 
    public static final String SQL_REPLY_SELECT = 
 	         "SELECT * FROM tb_reply WHERE board_id = ? ORDER BY reply_regDate DESC";
@@ -80,4 +71,31 @@ public class VO {
    public static final String SQL_REPLY_DELETE =
 
          "DELETE FROM tb_reply WHERE reply_id=?";
+   
+   //아이디 찾기
+   public static final String SQL_ID_SEARCH = 
+		   "SELECT USER_ID FROM TB_USER WHERE USER_NAME =? AND USER_PHONE=?";
+   
+   //비번찾기
+   public static final String SQL_PW_SEARCH = 
+		   "SELECT USER_PW FROM TB_USER WHERE USER_ID =? AND USER_NAME=? AND USER_EMAIL=?";
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
